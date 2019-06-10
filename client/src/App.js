8cf8f29f9406e1e4eec7a34ebd07549ac05b0889
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-10 21:14:16 +0400

    completed part 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
@@ -24,7 +24,7 @@ export default class App extends Component {
 				<div>
 					<SavedList list={this.state.savedList} />
 					<Route exact path="/" component={MovieList} />
-					<Route path="/:id" component={Movie} />
+					<Route path="/movies/:id" component={Movie} />
 				</div>
 			</Router>
 		);
